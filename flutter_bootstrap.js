cd550c4cebda74c5e43687ee66d1eca07782541f
Shovin Michel David <shovinmicheldavid1285@gmail.com>
2025-10-05 16:10:22 +0530

    updated sos

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2398662905"
+    serviceWorkerVersion: "3313585960"
   }
 });
